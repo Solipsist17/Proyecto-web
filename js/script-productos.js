6df481899a348b032418b2db183e2f12cf165371
Solipsist17 <sebas.17v@gmail.com>
2023-05-29 19:57:45 -0500

    agregando mejoras en la página de inicio y producto, falta terminar producto

--- js/script-productos.js
+++ js/script-productos.js
@@ -1,10 +1,41 @@
-const productoContainers = [...document.querySelectorAll(".producto-container")];
+/* Agregamos los productos dinámicamente */
+let productos = ["../img/Productos/mancuerna-20kg1.jpg", "../img/Productos/banco-ejercicios.jpg", "../img/Productos/bandas_elasticas.jpg", "../img/Productos/chaleco_de_peso.jpg", "../img/Productos/bicicleta.jpg", "../img/Productos/caminadora.jpg"];
 
+
+let container = document.querySelector(".producto-container");
+for (let i=0; i<productos.length; i++){
+    let cardDiv = document.createElement("div");
+    cardDiv.className = "producto-card";
+    let imgDiv = document.createElement("div");
+    imgDiv.className = "producto-img";
+    let descuentoSpan = document.createElement("span");
+    descuentoSpan.className = "descuento-tag";
+    descuentoSpan.textContent = "50% off";
+    let img = document.createElement("img");
+    img.className = "producto-miniatura";
+    img.src = productos[i];
+    let button = document.createElement("button");
+    button.className = "card-btn";
+    button.textContent = "Agregar al carrito";
+
+    imgDiv.appendChild(descuentoSpan);
+    imgDiv.appendChild(img);
+    imgDiv.appendChild(button);
+
+    cardDiv.appendChild(imgDiv);
+
+    container.appendChild(cardDiv);
+}
+
+
+/* Slider en productos  */
 /* let productoContainers = [];
 for(let i = 0; i<document.querySelectorAll(".producto-container").length; i++ ){
     let elemento = document.querySelectorAll(".producto-container")[i];
     productoContainers.push(elemento);
 } */
+
+const productoContainers = [...document.querySelectorAll(".producto-container")];
 
 const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
 const preBtn = [...document.querySelectorAll(".pre-btn")];
@@ -23,9 +54,38 @@ productoContainers.forEach((item, i) => {
     });
 });
 
+/*  */
+/* let mostrador = document.getElementById("producto");
+let seleccion = document.getElementById("seleccion");
+let imgSeleccioada = document.getElementById("img"); */
+
+
+function cargar(item) {
+    let body = document.body;
+    let mostrador = document.getElementById("producto");
+    let banner = document.getElementById("producto-banner");
+    let seleccion = document.getElementById("seleccion");
+    let imgSeleccionada = document.getElementById("img");
+
+    console.log("Se clickeó el botón");
+    /* mostrador.style.width = "60%"; */
+    seleccion.style.width = "40%";
+    seleccion.style.opacity = "1";
+
+    body.style.overflow = "hidden"; /* Bloqueamos el scroll */
+
+    mostrador.style.opacity = "0.4";
+    banner.style.opacity = "0.4";
+
+    let rect = item.getBoundingClientRect(); 
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let posicionVertical = scrollTop;
+    console.log(posicionVertical);
+    seleccion.style.top = posicionVertical + "px";
+}
 
 /*Rodrigo agrego esto*/
-let mostrador = document.getElementById("mostrador");
+/* let mostrador = document.getElementById("mostrador");
 let seleccion = document.getElementById("seleccion");
 let imgSeleccionada = document.getElementById("img");
 let modeloSeleccionado = document.getElementById("modelo");
@@ -60,5 +120,5 @@ function quitarBordes(){
     for(i=0;i <items.length; i++){
         items[i].style.border = "none";
     }
-}
+} */
 /*Hasta aca*/
