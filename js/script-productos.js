120a1773a4a78b976c5ef6b9833ccb416f1c172c
Solipsist17 <sebas.17v@gmail.com>
2023-06-05 02:41:52 -0500

    Productos terminado para la presentación del avance 3

--- js/script-productos.js
+++ js/script-productos.js
@@ -1,32 +1,10 @@
 
-/* Creamos los productos dinámicamente */
+/* Banner de anuncios */
 let productosBanner = ["../img/Productos/banner1.png", "../img/Productos/banner2.png", "../img/Productos/banner3.png"];
 
 let container = document.querySelector(".producto-container");
 
 for (let i=0; i<productosBanner.length; i++){
-    /* let cardDiv = document.createElement("div");
-    cardDiv.className = "producto-card";
-    let imgDiv = document.createElement("div");
-    imgDiv.className = "producto-img";
-    let descuentoSpan = document.createElement("span");
-    descuentoSpan.className = "descuento-tag";
-    descuentoSpan.textContent = "50% off";
-    let img = document.createElement("img");
-    img.className = "producto-miniatura";
-    img.src = productosBanner[i];
-    let button = document.createElement("button");
-    button.className = "card-btn";
-    button.textContent = "Agregar al carrito";
-
-    imgDiv.appendChild(descuentoSpan);
-    imgDiv.appendChild(img);
-    imgDiv.appendChild(button);
-
-    cardDiv.appendChild(imgDiv);
-
-    container.appendChild(cardDiv); */
-
     let cardDiv = document.createElement("div");
     cardDiv.className = "producto-card";
     cardDiv.style.width = "100%";
@@ -43,46 +21,125 @@ for (let i=0; i<productosBanner.length; i++){
     container.appendChild(cardDiv);
 }  
 
-/* Slider en productos  */
-
+/* Slider en banner de anuncios  */
 const productoContainers = [...document.querySelectorAll(".producto-container")];
 
 const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
 const preBtn = [...document.querySelectorAll(".pre-btn")];
        
 productoContainers.forEach((item, i) => {
-    let containerDimensiones = item.getBoundingClientRect();
-    let containerWidth = containerDimensiones.width;
-    /* console.log(i); */
-    /* let card = document.getElementsByClassName("producto-card")[0]; */
-    /* console.log(item.getElementsByClassName(".")); */
-    /* console.log(item.closest(".producto-card")); */
     console.log(item);
-    console.log(containerWidth);
+
     nxtBtn[i].addEventListener("click", () => {
-        item.scrollLeft += /* 800 */  containerWidth;
+        let banner = document.getElementById("producto-banner");
+        let containerWidth = banner.getBoundingClientRect().width;
+        console.log("Ancho del contenedor " + containerWidth);
+      
+        item.scrollLeft += containerWidth;
+        if (item.scrollLeft >= item.scrollWidth - containerWidth) {
+            item.scrollLeft = 0; // Vuelve al inicio cuando alcanza el final
+        }
     });
     
     preBtn[i].addEventListener("click", () => {
-    item.scrollLeft -= /* 800 */  containerWidth;
+        let banner = document.getElementById("producto-banner");
+        let containerWidth = banner.getBoundingClientRect().width;
+        console.log("Ancho del contenedor " + containerWidth);
+
+        item.scrollLeft -= containerWidth;
+        if (item.scrollLeft <= 0) {
+            item.scrollLeft = item.scrollWidth - containerWidth + 100; // + 100 para arreglar el ligero desfase de la última imagen
+        }
     });
 });
 
+// se ejecuta el slider
+function ejecutarNextBtn() {
+    let item = document.querySelector(".producto-container");
+    let banner = document.getElementById("producto-banner");
+        let containerWidth = banner.getBoundingClientRect().width;
+        console.log("Ancho del contenedor " + containerWidth);
+      
+        item.scrollLeft += containerWidth;
+        if (item.scrollLeft >= item.scrollWidth - containerWidth) {
+            item.scrollLeft = 0; // Vuelve al inicio cuando alcanza el final
+        }
+}
+setInterval(ejecutarNextBtn, 15000); // Ejecutamos cada 15 segundos
+
+
 /* Mostrador de productos */
-let productos = [
+let productosEquipamiento = [
     {nombre: "Mancuernas", descripcion: "Par de mancuernas de 20kg cada una", precio: 90, img: "../img/Productos/mancuerna-20kg1.jpg"},
     {nombre: "Banco ejercicios", descripcion: "Banco para realizar ejercicios", precio: 250, img: "../img/Productos/banco-ejercicios.jpg"},
-    {nombre: "Bandas elasticas", descripcion: "Bandas para ejercicios", precio: 20, img: "../img/Productos/bandas_elasticas.jpg"},
+    {nombre: "Bandas elásticas", descripcion: "Set de 5 bandas", precio: 20, img: "../img/Productos/bandas-elasticas.jpg"},
     {nombre: "Chaleco peso", descripcion: "Chaleco con pesos para 20kg máximo", precio: 100, img: "../img/Productos/chaleco_de_peso.jpg"},
-    {nombre: "bicicleta", descripcion: "Bicicleta para ejercicios", precio: 160, img: "../img/Productos/Bicicleta.jpg"},
+    {nombre: "bicicleta", descripcion: "Bicicleta para ejercicios", precio: 160, img: "../img/Productos/bicicleta-ejercicios.jpg"},
     {nombre: "Caminadora", descripcion: "Múltiples funcionalidades", precio: 300, img: "../img/Productos/caminadora.jpg"}
 ]
 
-let mostrador = document.getElementById("producto");
-cargarMostrador(productos);
-function cargarMostrador(productos) {
-    let productoMostrador = document.getElementById("productoMostrador");
+let productosSuplementos = [
+    {nombre: "Creatina", descripcion: "Creatina en polvo", precio: 60.00, img: "../img/Productos/creatina.jpg"},
+    {nombre: "Proteína", descripcion: "Proteína en polvo", precio: 65.90, img: "../img/Productos/proteina.jpg"}
+]
 
+let productosAccesorios = [
+    {nombre: "Rodilleras", descripcion: "Par de rodilleras adidas", precio: 35.90, img: "../img/Productos/rodilleras.jpg"},
+    {nombre: "Muñequeras", descripcion: "Par de muñequeras nike", precio: 20.90, img: "../img/Productos/muñequeras.jpg"},
+    {nombre: "Guantes", descripcion: "Par de guantes adidas", precio: 29.90, img: "../img/Productos/guantes.jpg"}
+]
+
+let productos = [...productosEquipamiento, ...productosSuplementos, ...productosAccesorios];
+
+let mostrador = document.getElementById("producto");
+
+let categorias = [...document.querySelectorAll(".categorias a")];
+
+for (let i = 0; i < categorias.length; i++) { // Asignamos los eventos a los link de categorías
+    categorias[i].addEventListener("click", () => {
+        seleccionarCategoria(categorias[i].id);
+    })
+}
+
+function seleccionarCategoria(categoria) {
+
+
+    switch (categoria) {
+        case "equipamiento": 
+            cargarMostrador(productosEquipamiento, categoria);
+            break;
+        case "suplementos": 
+            cargarMostrador(productosSuplementos, categoria);
+            break;
+        case "accesorios": 
+            cargarMostrador(productosAccesorios, categoria);
+            break;
+        default:   
+            alert("Categoría inválida");
+    }
+}
+
+//cargarMostrador(productos); /* Cargar el mostrador de acuerdo a la categoría seleccionada */
+function cambiarNumFilasMostrador(productoMostrador, cantidadProductos) {
+    let numFilas = (cantidadProductos/3);
+    let filas = Math.ceil(numFilas); // Redondeo al superior
+    productoMostrador.style.gridTemplateRows = `repeat(${filas}, 1fr)`;
+    console.log(`Número de filas del mostrador: ` + filas);
+}
+
+cargarMostrador(productos, "Todos"); // Cargamos todos los productos
+
+function cargarMostrador(productos, categoria) { 
+    let productoMostrador = document.getElementById("productoMostrador");
+    productoMostrador.innerHTML = ""; // Limpiamos el mostrador
+
+    let categoriaTitulo = document.querySelector(".producto-categoria"); 
+    categoriaTitulo.id = categoria;
+    categoriaTitulo.textContent = categoria;
+    
+    /* productoMostrador.style.gridTemplateRows = "repeat(5, 1fr)"; */
+    cambiarNumFilasMostrador(productoMostrador, productos.length); 
+        
     for (let i = 0; i < productos.length; i++) {
         let productoCard = document.createElement("div");
         productoCard.className = "producto-card";
@@ -138,10 +195,9 @@ function cargarMostrador(productos) {
         
 /* Carrito de compras */
 
-let numProductos = [];
 let productosCarrito = []; /* Productos agregados al carrito */
 
-function cargar(item) { /* Cargamos el carrito de compras */
+function cargar(item) { /* Mostramos el carrito de compras cuando se clickea en agregar carrito*/
 
     let body = document.body;
     let mostrador = document.getElementById("producto");
@@ -167,7 +223,7 @@ function cargar(item) { /* Cargamos el carrito de compras */
     banner.style.opacity = "0.4";
 
     /* Obtenemos las dimensiones y posicionamos */
-    let rect = item.getBoundingClientRect(); 
+    //let rect = item.getBoundingClientRect(); 
     let posicionVertical = window.pageYOffset || document.documentElement.scrollTop;
     console.log(posicionVertical);
     seleccion.style.top = posicionVertical + "px";
@@ -182,7 +238,7 @@ function cargar(item) { /* Cargamos el carrito de compras */
 function agregarCarrito(item) {
     let productCard = item.closest(".producto-card");
     productosCarrito.push(productCard); /* Agregamos los productos al array carrito */
-    console.log(productosCarrito);
+    /* console.log(productosCarrito); */
 }
 
 function quitarCarrito(index) {
@@ -192,7 +248,7 @@ function quitarCarrito(index) {
     cargarArrayCarrito(productosCarrito);
 }
 
-function cargarArrayCarrito (productosCarrito) {
+function cargarArrayCarrito (productosCarrito) { // Creamos los elementos del array carrito 
     let seleccionContainer = document.getElementById("seleccionContainer"); 
     seleccionContainer.innerHTML = ""; /* Limpiamos el contenedor */
     /* console.log(productosCarrito[0].querySelector(".precio").textContent); */
@@ -240,8 +296,23 @@ function cargarArrayCarrito (productosCarrito) {
     }
 
     /* AQUI ACTUALIZAR SUBTOTAL Y PRECIO DE ENVIO */
+    //let subTotal = document.querySelector(".subtotal");
+    calcularSubTotal(productosCarrito);
 }
 
+function calcularSubTotal(productosCarrito) {
+    let subTotal = 0;
+    let spanSubTotal = document.querySelector(".subtotal");
+    productosCarrito.forEach((item) => {
+        txtPrecio = item.querySelector(".precio").textContent.substring(2);
+        precio = parseFloat(txtPrecio);
+        subTotal += precio;
+        /* console.log(precio); */
+    });
+    console.log(subTotal.toFixed(2));
+    spanSubTotal.textContent = "";
+    spanSubTotal.textContent += "SubTotal: " + "S/" + subTotal.toFixed(2);
+}
 
 function cerrar() {
     let body = document.body;
@@ -254,42 +325,3 @@ function cerrar() {
     banner.style.opacity = "1";
     body.style.overflow = "auto";
 }
-
-/*Rodrigo agrego esto*/
-/* let mostrador = document.getElementById("mostrador");
-let seleccion = document.getElementById("seleccion");
-let imgSeleccionada = document.getElementById("img");
-let modeloSeleccionado = document.getElementById("modelo");
-let descripSeleccionada = document.getElementById("descripcion");
-let precioSeleccionado = document.getElementById("precio");
-
-function cargar(item){
-    quitarBordes();
-    mostrador.style.width = "60%";
-    seleccion.style.width = "40%";
-    seleccion.style.opacity = "1";
-    item.style.border = "2px solid red";
-
-    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
-
-    modeloSeleccionado.innerHTML =  item.getElementsByTagName("p")[0].innerHTML;
-
-    descripSeleccionada.innerHTML = "Descripción del modelo ";
-
-    precioSeleccionado.innerHTML =  item.getElementsByTagName("span")[0].innerHTML;
-
-
-}
-function cerrar(){
-    mostrador.style.width = "100%";
-    seleccion.style.width = "0%";
-    seleccion.style.opacity = "0";
-    quitarBordes();
-}
-function quitarBordes(){
-    var items = document.getElementsByClassName("item");
-    for(i=0;i <items.length; i++){
-        items[i].style.border = "none";
-    }
-} */
-/*Hasta aca*/
