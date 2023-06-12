8e20b3241d6cd579dd7ee9dedad747aa4f0a5128
waterbottle-1L <roamca14@gmail.com>
2023-06-11 21:58:04 -0500

    ACTUALIZACION
    AVISAR SI ES QUE HAY INCONVENIENTES CON EL PROYECTO
    
    ...XD

--- js/script-servicios.js
+++ js/script-servicios.js
@@ -1,6 +1,6 @@
 (function(){
     
-    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const sliders = [...document.querySelectorAll('.especialista__body')];
     const buttonNext = document.querySelector('#next');
     const buttonBefore = document.querySelector('#before');
     let value;   
@@ -14,17 +14,17 @@
     });
 
     const changePosition = (add)=>{
-        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
-        value = Number(currentTestimony);
+        const currentEspecialista = document.querySelector('.especialista__body--show').dataset.id;
+        value = Number(currentEspecialista);
         value+= add;
 
 
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
+        sliders[Number(currentEspecialista)-1].classList.remove('especialista__body--show');
         if(value === sliders.length+1 || value === 0){
             value = value === 0 ? sliders.length  : 1;
         }
 
-        sliders[value-1].classList.add('testimony__body--show');
+        sliders[value-1].classList.add('especialista__body--show');
 
     }
 
