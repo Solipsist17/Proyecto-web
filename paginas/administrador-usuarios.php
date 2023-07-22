b8ba4093ab57ab2275cffca6957931fc4fd3e241
waterbottle-1L <roamca14@gmail.com>
2023-07-21 22:10:07 -0500

    deco de usaers
    prototipo de vista users, falta traer losdatos de la db a la pagina

--- paginas/administrador-usuarios.php
+++ paginas/administrador-usuarios.php
@@ -1,120 +1,91 @@
-<?php 
-    session_start();
-    // Verificar si el usuario ha iniciado sesión
-    if (!isset($_SESSION['idUsuario'])) {
-        header("Location: login.php");
-        exit;
-    }
+<?php
 
-    require_once("../conexion/conexion.php");
-    // Función para obtener todos los artículos de la base de datos
-    //session_start();
-    function obtenerProductos() {
-        global $conn;
-        $sql = "SELECT idProducto, nombre, descripcion, precio FROM producto";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-        $productos = array();
-        while ($row = $result->fetch_assoc()) {
-        $productos[] = $row;
-        }
-        return $productos;
-        } else {
-        return array();
-        }
-    }   
+session_start();
+error_reporting(0);
+
+$validar = $_SESSION['nombre'];
+
+if( $validar == null || $validar = ''){
+
+  header("Location: ../paginas/cuenta.php");
+  die();
   
-    // Agregar un nuevo artículo
-    if ($_SERVER["REQUEST_METHOD"] == "POST" &&
-    isset($_POST["agregar"])) {
-    $nombre = $_POST["nombre"];
-    $descripcion = $_POST["descripcion"];
-    $precio = $_POST["precio"];
-/*     $imagen = $_POST["imagen"]; */
-    $sql = "INSERT INTO productos (nombre,
-    descripcion, precio)VALUES ('$nombre', '$descripcion', $precio,)";
-    $conn->query($sql);
-    }
+}
 
-    // Modificar un artículo existente
-    if ($_SERVER["REQUEST_METHOD"] == "POST" &&
-    isset($_POST["modificar"])) {
-    $idProducto = $_POST["idProducto"];
-    $nombre = $_POST["nombre"];
-    $descripcion = $_POST["descripcion"];
-    $precio = $_POST["precio"];
-/*     $imagen = $_POST["imagen"]; */
-    $sql = "UPDATE productos SET nombre='$nombre',
-    descripcion=$descripcion, precio='$precio'WHERE idProducto=$idProducto";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
     
-    $conn->query($sql);
-    }
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/fontawesome-all.min.css">
 
-        // Eliminar un artículo
-    if ($_SERVER["REQUEST_METHOD"] == "POST" &&
-    isset($_POST["eliminar"])) {
-    $idProducto = $_POST["idProducto"];
-    $sql = "DELETE FROM productos WHERE idProducto=$idProducto";
-    $conn->query($sql);
-    }
-    // Obtener todos los artículos
-    $productos = obtenerProductos();
+<link rel="stylesheet" href="../css/style-administrador-users.css">
+    <title>Usuarios</title>
+</head>
+
+<div class="container is-fluid">
+
+
+
+
+<div class="col-xs-12">
+  		<h1>Bienvenido Lector <?php echo $_SESSION['nombre']; ?></h1>
+      <br>
+		<h1>Lista de usuarios</h1>
+    <br>
+		<div>
+
+      <a class="btn btn-warning" href="../includes/_sesion/cerrarSesion.php">Log Out
+      <i class="fa fa-power-off" aria-hidden="true"></i>
+       </a>
+
+		</div>
+		<br>
+
+
+
+
+           <br>
+
+
+			</form>
+        
+        
+ 
+      <table class="table table-striped table-dark " id= "table_id">
+
+                   
+                         <thead>    
+                         <tr>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                        <th>Telefono</th>
+                        <th>Fecha</th>
+                        <th>Rol</th>
+                     
+                        </tr>
+                        </thead>
+                        <tbody>
+
+				<?php
+include_once("../conexion/conexion.php");
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
- <title>Productos</title>
- <link rel="stylesheet" type="text/css"
-href="../css/style-administrador-productos.css">
-</head>
-<body>
-    <a class="volver" href="administrador-indice.php">Volver</a>
-    <div>
-        <h1 class="a1">Productos</h1>
-    </div>
-    <br>
-    <br>
-    <!-- Formulario para agregar un nuevo artículo -->
-    <div class="a2">
-    <h2>Agregar producto</h2>
-    </div>
-    <div class="agr_prod" >
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <input type="text" name="nombre" placeholder="Nombre" required>
-    <input type="text" name="descripcion" placeholder="Descripcion" required>
-    <input type="number" step="0.01" name="precio" placeholder="Precio" required>
-    </select>
-    <input type="submit" name="agregar" value="Agregar">
-    </form>
-    </div>
-    
-<br><br><br>
-    <!-- Tabla de artículos -->
-    <div class="a2">
-    <h2 >Listado de productos</h2>
-    </div>
-    
-    <table>
-    <tr>
-    <th>ID</th> <th>Nombre</th> <th>Descripcion</th> <th>Precio</th>
-    
-    </tr>
-    <?php foreach ($productos as $fila) { ?> <tr>
-    <td><?php echo $fila['idProducto']; ?></td> <td><?php echo $fila['nombre']; ?></td>
-    <td><?php echo $fila['descripcion']; ?></td> <td><?php echo $fila['precio']; ?></td>
-    <td>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <input type="hidden" name="idProducto" value="<?php echo $fila['idProducto']; ?>">
-    <input type="submit" name="eliminar" value="Eliminar">
-    </form>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <input type="hidden" name="idProducto" value="<?php echo $fila['idProducto']; ?>">
-    <input type="text" name="nombre" value="<?php echo $fila['nombre']; ?>" required>
-    <input type="text" name="descripcion" value="<?php echo $fila['descripcion']; ?>" required>
-    <input type="number" step="0.01" name="precio" value="<?php echo $fila['precio']; ?>" required>
-    <input type="submit" name="modificar" value="Modificar"> </form> </td>
-    </tr> <?php } ?> </table>
 
-</body>
+
+
+
+
+
+	</body>
+  </table>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+<script src="../js/user.js"></script>
+
+
 </html>
