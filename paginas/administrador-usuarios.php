5d1ab07f3b55dd03ee30b355cbb4ff807109e1cf
waterbottle-1L <roamca14@gmail.com>
2023-07-23 13:22:16 -0500

    Consulta para traer datos OK
    Las consultas para traer los datos de usuario ya estan implementadas correctamente.

--- paginas/administrador-usuarios.php
+++ paginas/administrador-usuarios.php
@@ -1,91 +1,89 @@
 <?php
-
 session_start();
-error_reporting(0);
-
-$validar = $_SESSION['nombre'];
-
-if( $validar == null || $validar = ''){
-
-  header("Location: ../paginas/cuenta.php");
-  die();
-  
+// Verificar si el usuario ha iniciado sesión
+if (!isset($_SESSION['idUsuario'])) {
+    header("Location: login.php");
+    exit;
 }
+include_once("../conexion/conexion.php"); 
+//Obtener usuarios
+function obtenerUsuarios() {
+  global $conn;
+  $sql = "SELECT usuario.idUsuario, usuario.nombre AS nombreUsuario, usuario.apellido, usuario.email, rol.nombre AS nombreRol FROM usuario
+  LEFT JOIN rol ON usuario.idRol = rol.idRol";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+      $usuarios = array();
+  while ($row = $result->fetch_assoc()) {
+      $usuarios[] = $row;
+  }
+      return $usuarios;
+  } else {
+      return array();
+  }
+}   
 
-
+$usuarios = obtenerUsuarios();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/fontawesome-all.min.css">
-
 <link rel="stylesheet" href="../css/style-administrador-users.css">
     <title>Usuarios</title>
 </head>
-
+<body>
+<a class="volver" href="administrador-indice.php">Volver</a>
 <div class="container is-fluid">
-
-
-
-
 <div class="col-xs-12">
-  		<h1>Bienvenido Lector <?php echo $_SESSION['nombre']; ?></h1>
+  		<h1>Administrador Actual: </h1>
       <br>
 		<h1>Lista de usuarios</h1>
     <br>
 		<div>
-
-      <a class="btn btn-warning" href="../includes/_sesion/cerrarSesion.php">Log Out
       <i class="fa fa-power-off" aria-hidden="true"></i>
        </a>
-
 		</div>
 		<br>
-
-
-
-
            <br>
-
-
 			</form>
-        
-        
- 
       <table class="table table-striped table-dark " id= "table_id">
+        <thead>    
+            <tr>
+              <th>ID del Usuario</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>E-mail</th>
+              <th>Rol</th>
 
-                   
-                         <thead>    
-                         <tr>
-                        <th>Nombre</th>
-                        <th>Correo</th>
-                        <th>Telefono</th>
-                        <th>Fecha</th>
-                        <th>Rol</th>
-                     
-                        </tr>
-                        </thead>
-                        <tbody>
-
-				<?php
-include_once("../conexion/conexion.php");
-?>
+            </tr>
+          </thead>
+ 
+              
+          
+          <tbody>
+          <?php foreach ($usuarios as $fila) { ?> 
+            <tr>
+              <td><?php echo $fila['idUsuario']; ?></td>
+              <td><?php echo $fila['nombreUsuario']; ?></td>
+              <td><?php echo $fila['apellido']; ?></td>
+              <td><?php echo $fila['email']; ?></td>
+              <td><?php echo $fila['nombreRol']; ?></td>
+              <td>
 
 
 
+             </td>
+           </tr>
+           <?php } ?> 
 
 
 
-
+          </tbody>
+				
 	</body>
   </table>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
-<script src="../js/user.js"></script>
-
-
 </html>
